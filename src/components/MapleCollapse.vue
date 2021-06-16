e06a65de7e44e01c701b118406a01da956e66396
Maple <hufeng@mountslink.com>
2021-06-16 22:35:17 +0800

    ok

--- src/components/MapleCollapse.vue
+++ src/components/MapleCollapse.vue
@@ -99,11 +99,9 @@ export default {
             }),
             changeAccordion = (item: any) => {
                 const d = new Set();
-                let c = 1;
 
                 for (const [key, value] of Object.entries(collapse.value)) {
-                    // if ((value as any).collapsed) d.add(key);
-                    if (value.collapsed) d.add(key);
+                    if ((value as any).collapsed) d.add(key);
                 }
                 if (type.value === 'multiple') {
                     d.has(item.key + '')
